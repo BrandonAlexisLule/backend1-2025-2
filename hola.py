886f6f8a68affcc64eed38366283ffb7f4d5bd04
BrandonAlexisLule <al19760600@ite.edu.mx>
2025-10-10 18:15:46 -0700

    subiendo avance a git

--- hola.py
+++ hola.py
@@ -1,7 +1,18 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+#python dictionary
+data = {
+    "name":"Alice",
+    "age":"30",
+    "city":"New York",
+    "is_student": False,
+    "hobbies":["reading", "coding"]
+}
+
+#ejecutar código html con render_templates
+
 
 @app.route('/')
 def hello_world():
@@ -14,6 +25,16 @@ def saluda():
 @app.route('/json')
 def saludajson():
     return jsonify(data)
+
+@app.route('/pagina')
+@app.route('/pagina<name>')
+def pagina(name = None):
+    return render_template('pagina.html', person=name)
+
+if __name__=='__main__':
+    app.run()
+
+
 
 
 
---
+++ leer_excel.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel('normalizar data.xlsx')
+print(df)
